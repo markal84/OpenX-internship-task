d3016d3c336cddf293ed1eedcccf4239a333393a
Mar Kal <marc@example.com>
2023-04-19 17:54:54 +0200

    feat: implemented display categories and total prices

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -15,10 +15,34 @@ export default function Products() {
       })
   }, [])
 
+  const categories = products.reduce((acc, product) => {
+    const { category, price } = product
+
+    if(!acc[category]) {
+      acc[category] = 0
+    }
+
+    acc[category] += price
+    return acc
+  }, {})
+
+
+  const categoriesArr = Object.keys(categories).map((category) => ({
+    category,
+    totalValue: categories[category],
+  }))
+
   return (
     <div>
-      <button type='button'>Show Products</button>
-      <p>Products</p>
+      Products categories
+      <ul>
+        {categoriesArr.map((categories, idx) => {
+          const { category, totalValue } = categories
+          return (
+            <li key={idx}>Category: {category}<span>, &nbsp; Total value: {totalValue.toFixed(2)}</span></li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
