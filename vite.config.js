ab132c5eeaa98fbe7650835e04f575a658964b5a
Mar Kal <marc@example.com>
2023-04-21 17:45:13 +0200

    feat: prepare to deploy page

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/OpenX-internship-task/",
   plugins: [react()],
   test: {
     globals: true,
